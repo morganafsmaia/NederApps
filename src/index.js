67ead6956fee6788e0cbd731d452c0b92dc8491c
morganafsmaia <morganafmaia@gmail.com>
2018-08-30 12:05:56 +0200

    improving file structure in the folder

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('./src'));
+app.use(express.static('../public'));
 
 var server = app.listen(3000);
+console.log("listening on 3000");
